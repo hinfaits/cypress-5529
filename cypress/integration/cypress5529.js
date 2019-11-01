2f4c6e424b397c9377af6d0d93e0b0a33195a219
atsui <atsui@exansoftware.com>
2019-11-01 11:22:13 -0700

    project template

---
+++ cypress/integration/cypress5529.js
@@ -0,0 +1,9 @@
+/**
+ * https://github.com/cypress-io/cypress/issues/5529
+ */
+describe('try to repro cypress issue 5529', function () {
+    it('should do something', function() {
+        
+        
+    });
+});
